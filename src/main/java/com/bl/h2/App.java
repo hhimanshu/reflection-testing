b40dbc8db19d9ba21e12a500aeec62cbb7c7ede3
hhimanshu <harit.himanshu@yahoo.in>
2020-04-17 17:38:29 -0700

    Added tests based on Reflection

--- src/main/java/com/bl/h2/App.java
+++ src/main/java/com/bl/h2/App.java
@@ -1,10 +1,12 @@
+package com.bl.h2;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
 
     }
 
-    private int privateMethod(int[] numbers) {
+    private static int privateMethod(int[] numbers) {
         var sum = 0;
         for (int number : numbers) {
             sum += number;
