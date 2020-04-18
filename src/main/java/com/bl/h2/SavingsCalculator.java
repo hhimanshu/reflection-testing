38b483602b72c21c2b51a0985e6c059008628e5b
hhimanshu <harit.himanshu@yahoo.in>
2020-04-17 18:42:55 -0700

    Added a SavingsCalculator and number of test placeholders

---
+++ src/main/java/com/bl/h2/SavingsCalculator.java
@@ -0,0 +1,31 @@
+package com.bl.h2;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    public float calculate() {
+        return sumOfCredits() - sumOfDebits();
+    }
+
+    private float sumOfCredits() {
+        float sum = 0.0f;
+        for (float credit: credits) { sum += credit; }
+        return sum;
+    }
+
+    private float sumOfDebits() {
+        float sum = 0.0f;
+        for (float debit: debits) { sum += debit; }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/bl/h2/SavingsCalculatorTest.java
@@ -0,0 +1,41 @@
+package com.bl.h2;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public class SavingsCalculatorTest {
+
+    @Test
+    public void testSavingsCalculatorExists() {}
+
+    @Test
+    public void testExistenceOfPrivateFields() {}
+
+    @Test
+    public void testConstructor() {}
+
+    @Test
+    public void testFieldsValueSetWhenConstructorCalled() {}
+
+    @Test
+    public void testCalculateExists() {}
+
+    @Test
+    public void testSumOfCreditsExists() {}
+
+    @Test
+    public void testSumOfCreditsWorksCorrectly() {}
+
+    @Test
+    public void testSumOfDebitsExists() {}
+
+    @Test
+    public void testSumOfDebitsWorksCorrectly() {}
+
+    @Test
+    public void testCalculateWorksCorrectly() {}
+
+    @Test
+    public void testMainMethodPrintsCorrectOutput() {}
+}
