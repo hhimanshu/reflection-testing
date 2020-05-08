b2e308ec567df28b7b189385ba4eb1f67dff6ead
hhimanshu <harit.himanshu@yahoo.in>
2020-05-07 18:24:33 -0700

    Fix bug related to bestLoanRates

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -12,9 +12,9 @@ public class Finance {
     public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
     public static final Map<String, String> commandsToUsage = Map.of(
-            BEST_LOAN_RATES, "usage: BestLoanRates <numberOfYears>",
-            SAVINGS_CALCULATOR, "usage: SavingsCalculator <credits separated by ','> <debits separated by ','>",
-            MORTGAGE_CALCULATOR, "usage: MortgageCalculator <loanAmount> <termInYears> <annualRate>"
+            BEST_LOAN_RATES, "usage: bestLoanRates",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
 
     public static void main(String[] args) {
@@ -52,7 +52,7 @@ public class Finance {
     private static boolean validateCommandArguments(String[] args) {
         switch (args[0]) {
             case BEST_LOAN_RATES:
-                return args.length == 2;
+                return args.length == 1;
             case SAVINGS_CALCULATOR:
                 return args.length == 3;
             case MORTGAGE_CALCULATOR:
