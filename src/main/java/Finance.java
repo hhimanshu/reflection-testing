ff95a20e96427835bc91bae695d73028364080d6
hhimanshu <harit.himanshu@yahoo.in>
2020-05-07 17:02:34 -0700

    added more methods and usage guidelines for Finance class

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public class Finance {
 
     public static final Map<String, String> commandsToUsage = Map.of(
-            "bestLoanRates", "usage: Finance bestLoanRates <numberOfYears>",
+            "bestLoanRates", "usage: BestLoanRates <numberOfYears>",
             "savingsCalculator", "usage: SavingsCalculator <credits separated by ','> <debits separated by ','>",
             "mortgageCalculator", "usage: MortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
@@ -13,6 +13,29 @@ public class Finance {
         2. Call the different methods
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        final String command = args[0];
+        if (!commandsToUsage.containsKey(command)) {
+            System.out.println(command + ": command not found");
+            System.exit(-1);
+        }
+
+
+        final boolean isValidCommand = validateCommandArguments(args);
+        if (!isValidCommand) {
+            System.out.println(commandsToUsage.get(args[0]));
+            System.exit(-1);
+        }
+    }
+
+    private static boolean validateCommandArguments(String[] args) {
+        switch (args[0]) {
+            case "bestLoanRates":
+                return args.length == 2;
+            case "savingsCalculator":
+                return args.length == 3;
+            case "mortgageCalculator":
+                return args.length == 4;
+        }
+        return false;
     }
 }
--- src/test/java/FinanceTest.java
+++ src/test/java/FinanceTest.java
@@ -27,11 +27,45 @@ public class FinanceTest {
     @Disabled
     @Test
     public void testCommandsToUsage() {
-        /**
+        /*
          * 1. Existence of field
          * 2. isPublic, isFinal, isStatic, isMap (type)
          * 3. Has 3 entries
          * 4. Test all entries
          */
+    }
+
+    @Disabled
+    @Test
+    public void testMainCommandInput() {
+        /*
+         * 1. Pass command from the commandsToUsage
+         * 2. Test invalid command
+         */
+    }
+
+    @Disabled
+    @Test
+    public void testValidateCommandArgumentsExistence() {
+        /*
+         * 1. Method exists
+         * 2. isStatic, isPrivate, returns boolean
+         * 3. accepts String[] parameter
+         */
+    }
+
+    @Disabled
+    @Test
+    public void testValidateCommandArgumentsCorrectness() {
+        /*
+         * 1. Send all 3 valid argument[0] with different rest of argument length
+         * 2. Send invalid args[0] and assert that it should return false;
+         */
+    }
+
+    @Disabled
+    @Test
+    public void testMainWithValidCommandInvalidUsage() {
+
     }
 }
